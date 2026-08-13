0cb6551d8050d7b034e5f5dc0dcc61a57cd30fb0
estebanWarinet <estebanwarinet@gmail.com>
2026-08-13 14:37:25 -0300

    Riu Challenge - Primer commit con las configuraciones basicas, el primner docker con kafka y la base de datos local.

---
+++ src/test/java/com/estebanwarinet/hotelsearch/HotelAvailabilitySearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.estebanwarinet.hotelsearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelAvailabilitySearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
