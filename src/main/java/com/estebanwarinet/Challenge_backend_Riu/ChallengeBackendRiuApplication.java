029672940578e54868996de5c457a80a6148ad28
estebanWarinet <estebanwarinet@gmail.com>
2026-08-13 14:54:10 -0300

    Se actualizo el nombre del paquete y el nombre el pom que tenian errores porque falto agregar la version

---
+++ src/main/java/com/estebanwarinet/Challenge_backend_Riu/ChallengeBackendRiuApplication.java
@@ -0,0 +1,13 @@
+package com.estebanwarinet.Challenge_backend_Riu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallengeBackendRiuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChallengeBackendRiuApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/estebanwarinet/Challenge_backend_Riu/ChallengeBackendRiuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.estebanwarinet.Challenge_backend_Riu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengeBackendRiuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
